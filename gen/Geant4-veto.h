17af9c57a6c0080dd11d4d15e84fe52d118ff718
Pere Mato <pere.mato@cern.ch>
2023-02-21 17:39:18 +0100

    Temporary changes while investigating

--- gen/Geant4-veto.h
+++ gen/Geant4-veto.h
@@ -7,6 +7,10 @@ CLHEP::HepLorentzRotation CLHEP::operator*(const CLHEP::HepRotationY &, const CL
 CLHEP::HepLorentzRotation CLHEP::operator*(const CLHEP::HepRotationZ &, const CLHEP::HepLorentzRotation &)
 G4VUPLData::_theParticleIterator
 G4int G4UImanager::ApplyCommand(const G4String &)
-
-
-
+#G4bool G4String::operator!=(const char *)
+#G4bool G4String::operator!=(const G4String &)
+#G4bool G4String::operator==(const char *)
+#G4bool G4String::operator==(const G4String &)
+#void G4String::G4String(char)
+#void G4String::G4String(const char *, str_size)
+#void G4String::G4String(const char *)
