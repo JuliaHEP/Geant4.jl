0299ab088dc5006fe82d775e8e13004a4a809376
Pere Mato <pere.mato@cern.ch>
2024-01-13 19:53:54 +0100

    Protect make_callback that is called concurrently

--- src/G4CallBacks.jl
+++ src/G4CallBacks.jl
@@ -12,51 +12,60 @@ end
 #----Global vector (GC) ---------------------------------------------------------------------------
 callbacks = Vector{CallBack}()
 
-#----Function to create a callback avoiding the use of closures (ARM is not supported)
+#----Function to create a callback avoiding the use of closures (ARM is not supporting closures)
+#    We need to project with a lock the make_callback since it can be called from concurrent threads
+const spinlock = Base.Threads.SpinLock()
 function make_callback(ctx, fun, rt, args)
-    i_callback = Symbol(:_internal_callback_, Symbol(typeof(ctx)))
-    narg = length(args)
-    CONTEXT = typeof(ctx)
-    #---create the internal callback function------------------------------------------------------
-    if narg == 0
-        eval( 
-            quote
-                function $i_callback(data::Ptr{Cvoid})::$rt
-                    cb_data = unsafe_pointer_to_objref(data)::CallBackData{$CONTEXT}
-                    cb_data.callback(cb_data.context)
+    lock(spinlock)
+    try
+        i_callback = Symbol(:_internal_callback_, Symbol(typeof(ctx)))
+        narg = length(args)
+        CONTEXT = typeof(ctx)
+        #---create the internal callback function------------------------------------------------------
+        if narg == 0
+            eval( 
+                quote
+                    function $i_callback(data::Ptr{Cvoid})::$rt
+                        cb_data = unsafe_pointer_to_objref(data)::CallBackData{$CONTEXT}
+                        cb_data.callback(cb_data.context)
+                    end
                 end
-            end
-        )
-    elseif narg == 1
-        eval( 
-            quote
-                function $i_callback(a1::$(args[1]), data::Ptr{Cvoid})::$rt
-                    cb_data = unsafe_pointer_to_objref(data)::CallBackData{$CONTEXT}
-                    cb_data.callback(a1[], cb_data.context)
+            )
+        elseif narg == 1
+            eval( 
+                quote
+                    function $i_callback(a1::$(args[1]), data::Ptr{Cvoid})::$rt
+                        cb_data = unsafe_pointer_to_objref(data)::CallBackData{$CONTEXT}
+                        cb_data.callback(a1[], cb_data.context)
+                    end
                 end
-            end
-        )
-    elseif narg == 2
-        eval( 
-            quote
-                function $i_callback(a1::$(args[1]), a2::$(args[2]), data::Ptr{Cvoid})::$rt
-                    cb_data = unsafe_pointer_to_objref(data)::CallBackData{$CONTEXT}
-                    cb_data.callback(a1[], a2[], cb_data.context)
+            )
+        elseif narg == 2
+            eval( 
+                quote
+                    function $i_callback(a1::$(args[1]), a2::$(args[2]), data::Ptr{Cvoid})::$rt
+                        cb_data = unsafe_pointer_to_objref(data)::CallBackData{$CONTEXT}
+                        cb_data.callback(a1[], a2[], cb_data.context)
+                    end
                 end
-            end
-        )
-    else
-        error("callbacks with more than 2 arguments not yet supported")
+            )
+        else
+            error("callbacks with more than 2 arguments not yet supported")
+        end
+        #---Instantiate the callback-------------------------------------------------------------------
+        cb = eval(
+                quote
+                    CallBack{$CONTEXT}( @safe_cfunction($i_callback, $rt, ($(args...), Ptr{Cvoid})),
+                                        CallBackData{$CONTEXT}($ctx, $fun))
+                end
+            )
+        push!(callbacks, cb)
+        unlock(spinlock)
+        return cb
+    catch
+        unlock(spinlock)
+        rethrow()
     end
-    #---Instantiate the callback-------------------------------------------------------------------
-    cb = eval(
-            quote
-                CallBack{$CONTEXT}( @safe_cfunction($i_callback, $rt, ($(args...), Ptr{Cvoid})),
-                                    CallBackData{$CONTEXT}($ctx, $fun))
-            end
-        )
-    push!(callbacks, cb)
-    return cb
 end
 function closure(cb::CallBack)
     return (cb.cfunction, pointer_from_objref(cb.cbdata))
