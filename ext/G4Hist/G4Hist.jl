72f9a541b95ed5d3f074c9ad611ee930d419bbd4
Felix Hagemann <hagemann@mpp.mpg.de>
2024-08-19 11:37:48 +0200

    Update `G4Hist` to changes in `FHist@0.11`

--- ext/G4Hist/G4Hist.jl
+++ ext/G4Hist/G4Hist.jl
@@ -3,8 +3,8 @@ module G4Hist
     using FHist
     using Geant4.SystemOfUnits
 
-    const Hist1DType = typeof(Hist1D(bins=range(0,1,10)))
-    const Hist2DType = typeof(Hist2D(bins=(range(0,1,10),range(0,1,10))))
+    const Hist1DType = typeof(Hist1D(;binedges=range(0,1,10)))
+    const Hist2DType = typeof(Hist2D(;binedges=(range(0,1,10),range(0,1,10))))
     
     _getvalue(unit::Symbol) = getfield(Geant4.SystemOfUnits, unit)
     _getvalue(units::Tuple{Symbol,Symbol}) = (getfield(Geant4.SystemOfUnits, units[1]), getfield(Geant4.SystemOfUnits, units[2]))
@@ -19,7 +19,7 @@ module G4Hist
         unit::Symbol
         unitval::Float64
     end
-    Geant4.H1D(title, nbins, min, max, unit=:nounit) = H1D(title, Hist1D(Float64;bins=range(min,max,nbins+1),overflow=true), unit, _getvalue(unit))
+    Geant4.H1D(title, nbins, min, max, unit=:nounit) = H1D(title, Hist1D(;counttype=Float64,binedges=range(min,max,nbins+1),overflow=true), unit, _getvalue(unit))
 
     Base.push!(h::H1D, v, w=1) = push!(h.hist, v/h.unitval, w)
     Base.merge!(h1::H1D, h2::H1D) = merge!(h1.hist, h2.hist)
@@ -35,7 +35,7 @@ module G4Hist
         unit::Tuple{Symbol,Symbol}
         unitval::Tuple{Float64,Float64}
     end
-    Geant4.H2D(title, xbins, xmin, xmax, ybins, ymin, ymax, units=(:nounit, :nounit)) = H2D(title, Hist2D(Float64;bins=(range(xmin,xmax,xbins+1), range(ymin,ymax, ybins+1)), overflow=true), units, _getvalue(units))
+    Geant4.H2D(title, xbins, xmin, xmax, ybins, ymin, ymax, units=(:nounit, :nounit)) = H2D(title, Hist2D(;counttype=Float64,binedges=(range(xmin,xmax,xbins+1),range(ymin,ymax, ybins+1)),overflow=true), units, _getvalue(units))
 
     Base.push!(h::H2D, u, v, w=1) = push!(h.hist, u/h.unitval[1], v/h.unitval[2], w)
     Base.merge!(h1::H2D, h2::H2D) = merge!(h1.hist, h2.hist) 
