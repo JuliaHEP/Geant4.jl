ebb66208a625b4d99abe1a7ff326e74d79d39d5a
Pere Mato <pere.mato@cern.ch>
2024-11-26 14:02:22 +0100

    Removed examples from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Geant4.SystemOfUnits
 @testset "Geant4 tests" verbose = true begin 
     include("testGeometry.jl")
     include("testRandom.jl")
-    include("testExamples.jl")
+    # we do not need to run the examples since they are already tested in the CI while building the docs 
+    # include("testExamples.jl")
 end
 
